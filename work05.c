9e18cf16242c7046a3917425afd352fc767f8b25
Grace Chen <gchen20@stuy.edu>
2021-09-29 19:40:00 -0400

    declared and initilazed pointers

--- work05.c
+++ work05.c
@@ -5,7 +5,9 @@ int main(){
   int x = 2;
   long y = 3000000;
 
-  //int *p = &c;
+  char* cPointer = &c;
+  int* xPointer = &x;
+  long* yPointer = &y;
 
   printf("%c, %d, %ld \n", c, x, y);
   printf("address of %c is %p, address of %d is %p, address of %ld is %p \n", c, &c, x, &x, y, &y);
